c70e232c4cf730c11747bd52a2f2d038a4611846
Michael Jang <mjang01011@gmail.com>
2020-10-09 15:30:49 +0900

    'be'

---
+++ mini-amazon/backend/routes/userRoute.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import User from '../models/userModel';
+import { getToken, isAuth } from '../util';
+
+const router = express.Router();
+
+
+//signin
+router.post('/signin', async (req, res) => {
+  const signinUser = await User.findOne({
+    //check email and password
+    email: req.body.email,
+    password: req.body.password,
+  });
+  //if email & passowrd exists, send back data to front end
+  if (signinUser) {
+    res.send({
+      uid: signinUser.uid,
+      fullName: signinUser.fullname,
+      username: signinUser.username,
+      email: signinUser.email,
+      isSeller: signinUser.isSeller,
+      isAdmin: signinUser.isAdmin,
+      token: getToken(signinUser),
+    });
+  } else {
+    res.status(401).send({ message: 'Invalid Email or Password.' });
+  }
+});
+
+//create admin
+router.get('/createadmin', async (req, res) => {
+  try {
+    const user = new User({
+      fullname: 'Admin',
+      username: 'Admin',
+      email: 'admin@gmail.com',
+      password: 'admin',
+      secret_question: 'Course number:',
+      isSeller: false,
+      isAdmin: true,
+      balance: 0,
+    });
+    const newUser = await user.save();
+    res.send(newUser);
+  } catch (error) {
+    res.send({ message: error.message });
+  }
+});
+
+export default router;
