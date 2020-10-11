93457bd802f93c1c4c2adbee18b2df4f94f3d36d
James Leong <jameszleong@gmail.com>
2020-10-11 11:32:58 +0800

    added isSeller and isAdmin auth

--- mini-amazon/backend/util.js
+++ mini-amazon/backend/util.js
@@ -1,6 +1,8 @@
-//add isAuth, isSeller, isAdmin
+//add isAuth, isSeller, isAdmin for authentication tokens
+
 import jwt from 'jsonwebtoken';
 import config from './config';
+
 const getToken = (user) => {
     return jwt.sign({
         uid: user.uid,
@@ -13,6 +15,35 @@ const getToken = (user) => {
     });
 }
 
-export {
-    getToken
-}
+const isAuth = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        const onlyToken = token.slice(7, token.length);
+        jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+            if (err) {
+                return res.status(401).send({ message: 'Invalid Token' });
+            }
+            req.user = decode;
+            next();
+            return;
+        });
+    } else {
+        return res.status(401).send({ message: 'Token is not supplied.' });
+    }
+};
+
+const isSeller = (req, res, next) => {
+    if (req.user && req.user.isSeller) {
+        return next();
+    }
+    return res.status(401).send({ message: 'User is not a seller.' });
+};
+
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        return next();
+    }
+    return res.status(401).send({ message: 'User does not have admin privileges.' });
+};
+
+export { getToken, isAuth, isSeller, isAdmin };
