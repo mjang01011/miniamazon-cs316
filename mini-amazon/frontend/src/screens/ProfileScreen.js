9e11f25da9099d0d19cdf3e38360d52a0683ceae
acxu <anna.xu@duke.edu>
2020-11-13 11:26:29 -0500

    upload avatar

--- mini-amazon/frontend/src/screens/ProfileScreen.js
+++ mini-amazon/frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {logout, getUserBalance} from '../actions/userActions';
 import { useDispatch, useSelector } from 'react-redux';
 
 
 function ProfileScreen(props) {
     const userSignin = useSelector((state) => state.userSignin);
+    const [avatar, setAvatar] = useState('');
+    const [uploading, setUploading] = useState(false);
     const { userInfo } = userSignin;
     const userDetails = useSelector((state) => state.userDetails);
     // const { user } = userDetails;
@@ -17,6 +20,27 @@ function ProfileScreen(props) {
             //
         };
     }, []);
+
+    const uploadFileHandler = (e) => {
+      const file = e.target.files[0];
+      const bodyFormData = new FormData();
+      bodyFormData.append('avatar', file);
+      setUploading(true);
+      axios
+        .post('/api/upload/image', bodyFormData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((response) => {
+          setAvatar(response.data.fileLink);
+          setUploading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setUploading(false);
+        });
+    };
 
     const handleBalance = () => {
      props.history.push("/balance");
@@ -34,9 +58,23 @@ function ProfileScreen(props) {
     return <div className="profile">
         <li>
           <h1>
-            Avatar: 
+            Avatar: <img className="product-image" src={userInfo.avatar}></img>
           </h1>
         </li>
+        {/*
+        <li>
+          <label htmlFor="avatar">Upload Avatar</label>
+                <input
+                  type="text"
+                  name="avatar"
+                  value={avatar}
+                  id="avatar"
+                  onChange={(e) => setAvatar(e.target.value)}
+                ></input>
+                <input type="file" onChange={uploadFileHandler}></input>
+                {uploading && <div>Uploading...</div>}
+        </li>
+        */}
         <li>
           <h1>
             Full Name: {userInfo.fullName}
