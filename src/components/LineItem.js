aebc3f0120baeb9d84f1c2d6fea0d2fd90479281
James Leong <jameszleong@gmail.com>
2020-11-18 00:50:38 +0800

    minor fixes

--- src/components/LineItem.js
+++ src/components/LineItem.js
@@ -9,12 +9,12 @@ function LineItem({transaction}) {
             <p>Time of Transaction: {transaction.createdAt.split("T")[0] + " " + transaction.createdAt.split("T")[1].split(".")[0]}</p>
             {transaction.transactedItems.map(item=> {
                 return (<div className={"individual-item"} key={item._id}>
-                    <h4><a href={window.location.href.split("/")[0]+"product/"+item.item._id}>{item.item.itemName}</a></h4>
+                    <h4>{item.item.itemName}</h4>
                     <p>{item.item.description}</p>
                     <img className={"order-image"} src={item.item.image}/>
                     <p>Sold By: {item.seller.username}</p>
                     <p>Quantity: {item.quantity}</p>
-                    <p>Price: {item.price}</p>
+                    <p>Price: {item.price.toFixed(2)}</p>
                     <p>Purchase Subtotal: {(item.quantity * item.price).toFixed(2)}</p>
                 </div>)})}
         </div>
